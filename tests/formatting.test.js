4393ab00ea8a691a07554850a49f9a74a91c18ad
mitermayer <mitermayer.reis@gmail.com>
2018-11-18 08:46:37 +0000

    Adding basic formatting tests

--- tests/formatting.test.js
+++ tests/formatting.test.js
@@ -7,6 +7,34 @@ const PORT = 1337;
 let server;
 let remote;
 
+const getBufferContents = async remote =>
+  (await remote.call('getline', [1, '$'])).join('\n');
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const waitUntil = (condition, timeout = 4000) => {
+  return new Promise(resolve => {
+    let isTimedOut = false;
+    let timeoutId = null;
+
+    const check = () => {
+      const promise = condition();
+      promise.then(result => {
+        if (!isTimedOut && result === true) {
+          clearTimeout(timeoutId);
+          resolve();
+        } else if (!isTimedOut) {
+          check();
+        }
+      });
+    };
+
+    timeoutId = setTimeout(() => {
+      isTimedOut = true;
+      resolve();
+    }, timeout);
+  });
+};
+
 beforeAll(async () => {
   server = new Server();
   await server.listen(HOST, PORT);
@@ -16,6 +44,8 @@ afterAll(async () => {
   await server.close();
 });
 
+// should ensure that we cache original fixture contents and
+// restore it on the afterEach
 beforeEach(async () => {
   remote = new HeadlessRemoteClient({host: HOST, port: PORT});
   await remote.connect(server);
@@ -23,11 +53,61 @@ beforeEach(async () => {
 
 afterEach(async () => {
   if (remote.isConnected()) {
-    await remote.close();
+    try {
+      const filename =  await remote.call("expand", ["%:p"]);
+
+      if (filename) {
+        // restore the file
+        await remote.execute('earlier 1d | noautocmd | write');
+      }
+
+    } catch(e) {} finally {
+      await remote.close();
+    }
   }
 });
 
 test('PrettierVersion returns pluggin version', async () => {
   const result = await remote.execute('PrettierVersion');
   expect(result).toMatchSnapshot();
+});
+
+test('Prettier formats javascript file', async () => {
+  await remote.edit(
+    '/home/miter/repo/mitermayer/vim-prettier/tests/fixtures/javascript/foo.js',
+  );
+
+  const lines = await getBufferContents(remote);
+
+  // run sync formatting
+  await remote.execute('Prettier');
+
+  const updatedLines = await getBufferContents(remote);
+
+  // we now check that we have indeed formatted the code
+  expect(updatedLines).not.toBe(lines);
+});
+
+test('Prettier formats javascript file Async', async () => {
+  await remote.edit(
+    '/home/miter/repo/mitermayer/vim-prettier/tests/fixtures/javascript/foo.js',
+  );
+
+  const lines = await getBufferContents(remote);
+
+  // run async formatting
+  await remote.execute('PrettierAsync');
+
+  const unmodifiedLines = await getBufferContents(remote);
+
+  // async should not happen immediatly so content should still remain the same
+  expect(unmodifiedLines).toBe(lines);
+
+  // we now will wait until prettier has finally updated the content async
+  await waitUntil(async () => (await getBufferContents(remote)) !== lines);
+
+  const updatedLines = await getBufferContents(remote);
+
+  // we now check that we have indeed formatted the code
+  expect(lines).not.toBe(updatedLines);
 });
